03f51b5da1f35a98d31dd4e5b8e515b1b70c40fd
VladimirMonin <vladimir.monin2016@yandex.ru>
2024-12-14 14:50:26 +0500

    lesson_12: множественные аргументы функций *args

--- lesson_12.py
+++ lesson_12.py
@@ -38,83 +38,6 @@ Python функции. Аргументы и модули. Библиотеки 
 
     """
 
-"""
-DRY - Don't repeat yourself
-Принцип DRY (Don't Repeat Yourself) - это принцип разработки программного обеспечения, который гласит, что каждая часть программы должна быть написана только один раз и должна быть доступна для использования в других частях программы.
-
-SRP - Single Responsibility Principle
-Принцип единственной ответственности (Single Responsibility Principle, SRP) - это принцип разработки программного обеспечения, который гласит, что каждый класс или модуль должен иметь только одну ответственность или задачу.
-
-KISS (Keep It Simple, Stupid) - делай вещи проще. Функция должна быть простой и понятной.
-
-YAGNI (You Ain't Gonna Need It) - не пиши код, который сейчас не нужен. Добавляй функциональность по мере необходимости.
-"""
-
-"""
-Типы аргументов функций:
-1. Позиционные аргументы:
-    - Аргументы, передаваемые в функцию в том порядке, в котором они определены в объявлении функции.
-    - Количество позиционных аргументов должно соответствовать количеству параметров в объявлении функции.
-"""
-
-
-def say_hello(name: str, age: int) -> None:
-    """
-    Функция приветствия.
-    :param name: Имя пользователя
-    :param age: Возраст пользователя
-    """
-    print(f"Привет, {name.title()}! Тебе {age} лет.")
-
-
-say_hello("Алексей", 30)  # позиционные аргументы
-# say_hello(30, "Алексей")
-
-# message = say_hello("Алексей", 30)
-
-
-"""
-Типы аргументов функций:
-2. Именованные аргументы:
-    - Аргументы, передаваемые в функцию с указанием имен параметров.
-    - Именованные аргументы могут быть переданы в любом порядке.
-"""
-
-say_hello(age=20, name="Кристина")
-
-
-"""
-Типы аргументов функций:
-3. Аргументы по умолчанию:
-    - Аргументы, которые имеют значение по умолчанию, если они не переданы при вызове функции.
-    - Если аргумент не передан, используется значение по умолчанию.
-"""
-
-
-def is_adult(age:int, adult_age: int = 18) -> bool:
-    """
-    Функция проверки возраста.
-    :param name: Имя пользователя
-    :param age: Возраст пользователя
-    :return: True, если пользователь совершеннолетний, False в противном случае
-    """
-    return age >= adult_age
-
-
-result = is_adult(20)
-result2 = is_adult(17, 16)
-result3 = is_adult(age=20, adult_age=16)
-
-
-# PRACTICE Функция проверки на палиндром
-"""
-Напишите функцию которая принемает строку и возвращает bool значение является ли строка палиндромом
-Используйте аннотацию типов и документацию для функции
-Напишите 1 проверку.
-
-Срез
-[::-1]
-"""
 
 def is_palindrome(text: str) -> bool:
     """
@@ -130,21 +53,41 @@ def is_palindrome(text: str) -> bool:
     return ready_string == ready_string[::-1]
 
 
-import string
+"""
+Типы аргументов функций:
+4. *args и **kwargs:
+    - *args (звездочка и аргументы) - это способ передачи произвольного количества позиционных аргументов в функцию.
+    - **kwargs (две звездочки и аргументы) - это способ передачи произвольного количества именованных аргументов в функцию.
+"""
 
-def is_palindrome2(text: str) -> bool:
+product_list = ["хлеб", "молоко", "яйца", "сыр"]
+
+product1, product2, *other = product_list
+print(product1, product2, other)
+
+
+new_product = [product_list[0], product_list[1], product_list[2], product_list[3]]
+
+new_product = [*product_list]
+print(new_product)
+new_product = (*product_list, "банан")
+print(new_product)
+
+
+print("Hello")
+print("Hello", "My name is", "Alex")
+
+
+def is_palindrome2(*strings: str) -> list[bool]:
     """
-    Функция проверки на палиндром с использованием translate
-    
-    Arguments:
-        text -- строка которую нужно проверить
-        
-    Returns:
-        bool -- True если строка палиндром, False если нет
-        
-    Example:
-        >>> is_palindrome("А роза упала на лапу Азора!")
-        True
+    Функция проверки на палиндром
     """
-    clean_text = text.lower().translate(str.maketrans("", "", string.punctuation + " "))
-    return clean_text == clean_text[::-1]
+    result_list = []
+    for string in strings:
+        ready_string = string.lower().replace(" ", "").replace(",", "").replace(".", "")
+        result_list.append(ready_string == ready_string[::-1])
+
+    return result_list
+
+
+print(is_palindrome2("hello", "world", "шалаш", "а роза упала на лапу азора"))
