83def4f3570de3e7b801eb14897ade50a2692f56
VladimirMonin <vladimir.monin2016@yandex.ru>
2024-12-21 13:15:17 +0500

    hw_2: вторая точка входа с консолью и вводом города

---
+++ hw/hw_2/console_weather.py
@@ -0,0 +1,27 @@
+# Этот импорт неплох, но нам придется обращаться к элементам через название модуля
+# import utils
+
+# utils.get_weather()
+# utils.CITY
+
+# Плох. Потому что мы не знаем сколько и что импортируем.
+# from utils import *
+# get_weather()
+# CITY
+
+# Хорошо. Мы знаем, что импортируем. И даже что используется а что нет.
+from utils import get_weather, format_weather_message, notify_weather
+
+# Напишем собственную функцию main где город можно вводить через input
+
+
+def main():
+    city = input("Введите название города: ")
+    weather_dict = get_weather(city=city)
+    message = format_weather_message(weather_dict)
+    print(message)
+    input("Нажмите Enter для уведомления")
+    notify_weather(message)
+
+
+main()
