17b0855ea6bf44a7fbfacbc4520b298cc715bdd2
VladimirMonin <vladimir.monin2016@yandex.ru>
2025-01-11 14:31:17 +0500

    lesson_17: валидация для set_salary приватного атрибута

--- lesson_17.py
+++ lesson_17.py
@@ -10,14 +10,28 @@ Lesson 17
 """
 
 class Employee:
-    def __init__(self, name:str, age: int, salary:int) -> None:
+    def __init__(self, name:str, age: int, salary:int, threshold_percent_salary: int = 50) -> None:
         self.name = name
         self._age = age
         self.__salary = salary
+        self.__threshold_percent_salary: int = 50
 
     def get_salary(self) -> int:
         return self.__salary
     
     def set_salary(self, value: int) -> None:
+        if not isinstance(value, int):
+            raise TypeError("Зарплата должна быть числом")
+        if value < 0:
+            raise ValueError("Зарплата не может быть меньше 0")
+        # Проверим чтобы зарплата не колебалась более чем на __threshold_percent_salary
+        if abs(self.__salary - value) > self.__salary * self.__threshold_percent_salary / 100:
+            raise ValueError(f"Зарплата не может колебаться более чем на {self.__threshold_percent_salary}%")
+        
         self.__salary = value
+        
 
+manager = Employee("Владимир", 30, 100000)
+print(manager.get_salary())
+manager.set_salary("пирожок")
+print(manager.get_salary())
