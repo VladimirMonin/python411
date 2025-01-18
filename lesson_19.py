46d639e91c55dac3ca25cf416dd9a0b076f80585
VladimirMonin <vladimir.monin2016@yandex.ru>
2025-01-18 12:35:53 +0500

    lesson_19: наследование инициализатора атрибутов и методов родителя

--- lesson_19.py
+++ lesson_19.py
@@ -4,19 +4,32 @@ Lesson 19
 
 Тема: ООП Ч4. Наследование. Abstractmethod. Super. Переопределение и расширение. Урок: 19
 - Базовый пример на следования
+- __str__ - метод, который возвращает строку с описанием объекта
 """
 
 class Animal:
-    pass
+    def __init__(self, name: str):
+        self.name = name
+        print("Инициализация животного")
+    
+    def voice(self):
+        print("Животное издает звук")
+
+    def __str__(self):
+        return f"Животное по имени {self.name}"
 
 
 class Dog(Animal):
     pass
 
 
-dog = Dog()
+dog = Dog("Шарик")
 print(dog)
 print(type(dog))
 
-# <__main__.Dog object at 0x00000165C1236F90>
+dog.voice()
+
+# Инициализация животного
+# Животное по имени Шарик
 # <class '__main__.Dog'>
+# Животное издает звук
