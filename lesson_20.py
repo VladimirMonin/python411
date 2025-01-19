74c4ace5c3691de89ef13cca016cf3927ac6e8f9
VladimirMonin <vladimir.monin2016@yandex.ru>
2025-01-19 13:55:10 +0500

    lesson_20: Пример иерархического наследования с вызовом инициализаторов предков и передачей атрибутов через словарь и **kwargs

--- lesson_20.py
+++ lesson_20.py
@@ -3,38 +3,43 @@
 Тема: ООП Ч5. Наследование. Множественное. Иерархическое. MRO. Урок: 20
 - Взаимозависимость классов
 - Вызов метода наследника через родителя
+- Пример иерархического наследования с вызовом инициализаторов предков и передачей атрибутов
+- Пример иерархического наследования с вызовом инициализаторов предков и передачей атрибутов через словарь и **kwargs
 """
 
 # Цепочка наследования A - B - C
+# Альтернативный вариант
+# Много минусов. Потому что сложно отследить типы данных, сами данные и т.п.
 
 class A:
-    def __init__(self, attr_a:str):
+    def __init__(self, **kwargs):
         print("Инициализация класса A")
-        self.attr_a = attr_a
+        self.attr_a = kwargs.get('attr_a')
     
     def method_a(self):
         print("Метод A")
 
 class B(A):
-    def __init__(self, attr_a:str, attr_b:str):
-        super().__init__(attr_a)
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
         print("Инициализация класса B")
-        self.attr_b = attr_b
+        self.attr_b = kwargs.get('attr_b')
     
     def method_b(self):
         print("Метод B")
 
 class C(B):
-    def __init__(self,  attr_a:str, attr_b:str, attr_c: str):
-        super().__init__(attr_a, attr_b)
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
         print("Инициализация класса C")
-        self.attr_c = attr_c
+        self.attr_c = kwargs.get('attr_c')
     
     def method_c(self):
         print("Метод C")
 
+# Создание экземпляра с передачей всех атрибутов через kwargs
+c = C(attr_a="A", attr_b="B", attr_c="C")
 
-c = C("A", "B", "C")
 c.method_a()
 c.method_b()
 c.method_c()
