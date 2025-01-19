1d3556d0fc87ebf0494d5c18b81236faef31f724
VladimirMonin <vladimir.monin2016@yandex.ru>
2025-01-19 13:06:15 +0500

    lesson_20: базовый пример цепочки иерархического наследования

--- lesson_20.py
+++ lesson_20.py
@@ -5,91 +5,24 @@
 - Вызов метода наследника через родителя
 """
 
-from abc import ABC, abstractmethod
+# Цепочка наследования A - B - C
+
+class A:
+    def __init__(self):
+        print("Инициализация класса A")
+    pass
+
+class B(A):
+    def __init__(self):
+        super().__init__()
+        print("Инициализация класса B")
+    pass
+
+class C(B):
+    def __init__(self):
+        super().__init__()
+        print("Инициализация класса C")
+    pass
 
 
-class AbstractDocument(ABC):
-    def __init__(self, file_path: str) -> None:
-        self.file_path = file_path
-        self.file = self.open()
-    
-    @abstractmethod
-    def open(self):
-        pass
-
-    @abstractmethod
-    def read(self):
-        pass
-
-    @abstractmethod
-    def append(self):
-        pass
-    
-    @abstractmethod
-    def write(self):
-        pass
-
-    def __str__(self) -> str:
-        return f"{self.__class__.__name__} - {self.file_path}"
-    
-
-class TxtDocument(AbstractDocument):
-    def __init__(self, file_path: str) -> None:
-        super().__init__(file_path)
-
-    def open(self):
-        with open(self.file_path, "r", encoding="utf-8") as file:
-            return file.read()
-        
-    def read(self):
-        pass
-
-    def append(self):
-        pass
-
-    def write(self):
-        pass
-
-
-class MarkdownDocument(AbstractDocument):
-    def __init__(self, file_path: str) -> None:
-        super().__init__(file_path)
-
-    def open(self):
-        pass
-    def read(self):
-        pass
-    def append(self):
-        pass
-    def write(self):
-        pass
-
-
-class PdfDocument(AbstractDocument):
-    def __init__(self, file_path: str) -> None:
-        super().__init__(file_path)
-
-    def open(self):
-        pass
-    def read(self):
-        pass
-    def append(self):
-        pass
-    def write(self):
-        pass
-
-
-# abstract = AbstractDocument() # Это не работает. Мы не можем создать экземпляр абстрактного класса
-md_file = MarkdownDocument("file.md")
-pdf_file = PdfDocument("file.pdf")
-
-print(md_file)
-print(pdf_file)
-
-# MarkdownDocument - file.md
-# PdfDocument - file.pdf
-
-
-file ="lesson_14.txt"
-txt_file = TxtDocument(file)
-print(txt_file.file)
+c = C()
