4cb4123605727640728bbe9ad7ed1612e0f93230
VladimirMonin <vladimir.monin2016@yandex.ru>
2025-02-02 13:17:21 +0500

    lesson_24: списковые выраения и filter

--- lesson_24.py
+++ lesson_24.py
@@ -2,7 +2,7 @@
 Тема: Функции. Анонимные функции. Map Filter Sorted. Урок: 24
 - синтаксис анонимных функций
 """
-from marvel import small_dict, full_dict
+from marvel import small_dict, full_dict, simple_set
 
 def foo_0(x):
     return x + 10
@@ -11,3 +11,40 @@ foo = lambda x: x + 10
 
 print(foo_0(10))
 print(foo(10))
+
+# 1. Сделаю список из Simple set
+simple_list = list(simple_set)
+
+# 2. Обход циклом simple_list
+new_simple_list = []
+
+for film in simple_list:
+    new_simple_list.append(film)
+
+# list comprehension
+new_simple_list = [film for film in simple_list]
+
+
+# 3. Мы хотим это отфильтровать (фильмы с "чел" в названии)
+result_list_3 = []
+
+for film in simple_list:
+    if 'чел' in film.lower():
+        result_list_3.append(film)
+
+# list comprehension
+result_list_3= [film for film in simple_list if 'чел' in film.lower()]
+
+# filter - функция высшего порядка.
+# Высшего порядка - значит она принемает в себя другую функцию.
+# Принемает 2 аргумента: функцию и итерируемый объект.
+# Функция должна вернуть булево
+
+def search_string(string):
+    return "чел" in string.lower()
+
+result_list_3 = list(filter(search_string, simple_list))
+
+result_list_3 = list(filter(lambda film: "чел" in film.lower(), simple_list))
+
+print(result_list_3)
