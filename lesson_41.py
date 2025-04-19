87fd9af78b785eb3a6fa4850db7f0f610d50e497
VladimirMonin <vladimir.monin2016@yandex.ru>
2025-04-19 14:46:17 +0500

    lesson_41: добавлены новые запросы для выборки студентов по различным критериям и обновление данных студента через PeeWee ORM

--- lesson_41.py
+++ lesson_41.py
@@ -255,20 +255,97 @@ class Student(pw.Model):
 
 # 7. Сделаем тестовый запуск - получим всех студентов и выведем их на экран
 all_students = Student.select()
-print(type(all_students))  # <class 'peewee.ModelSelect'>
-print(all_students)  # <ModelSelect: Student>
-print(all_students[0])  # <Student: 1>
-print(all_students[0].first_name)  # Артём
-print(all_students[0].last_name)  # Серпентьев
 
-# Даже JOIN чтобы найти группу!!!
-print(all_students[0].group.group_name)  # python411
+
+# 8. Найдем студента с именем "Ольга" и выведем его на экран
+student = Student.get(Student.first_name == "Ольга")
+
+print(student.first_name, student.group, student.group.group_name)
+
+# 9. Всех студентов старше 20 лет
+all_students = Student.select().where(Student.age > 20)
 
 for student in all_students:
-    print(
-        student.first_name,
-        student.middle_name,
-        student.last_name,
-        student.age,
-        student.group.group_name if student.group else None,
-    )
+    print(student.first_name, student.age)
+
+# 10. Накинем на прошлую выборку фильтр - имена где есть "ан"
+all_students = Student.select().where(Student.first_name.contains("ан"))
+
+for student in all_students:
+    print(student.first_name, student.age)
+
+# 11. Найдем всех студентов группы "python411"
+all_students = Student.select().join(Group).where(Group.group_name == "python411")
+
+for student in all_students:
+    print(student.first_name, student.age)
+
+# 12. Найдем всех студентов группы "python411" и старше 20 лет
+all_students = Student.select().join(Group).where(
+    (Student.group.group_name == "python411") & (Student.age > 20)
+)
+
+for student in all_students:
+    print(student.first_name, student.age, student.group.group_name)
+
+
+"""
+Select - инструмент возвращающий данные из базы данных в виде коллекции объектов
+Where - фильтрует данные по заданному условию
+Join - объединяет таблицы по заданному условию, в отличии от Django ORM мы явно должны указать по какому полю мы соединяем таблицы
+
+Операторы:
+    | - ИЛИ
+    & - И
+    
+
+
+~ (тильда) - это логическое НЕ (NOT). Например: ~(Student.age > 20) - найдёт всех студентов, возраст которых НЕ больше 20 лет.
+
+.contains() - проверяет, содержит ли строка подстроку. Например: Student.first_name.contains("Алек") - найдёт всех "Александров" и "Алексеев".
+
+.startswith() и .endswith() - проверяют начало и конец строки. Например: Student.last_name.endswith("ов") - найдёт всех с фамилиями, оканчивающимися на "ов".
+
+.in_() - проверяет, входит ли значение в список. Например: Student.age.in_([18, 19, 20]) - найдёт студентов определённых возрастов.
+
+.not_in() - проверяет, что значение НЕ входит в список.
+
+.is_null(True) и .is_null(False) - проверяют, является ли поле NULL или NOT NULL. Например: Student.middle_name.is_null(True) - найдёт студентов без отчества.
+
+.between() - проверяет, находится ли значение в диапазоне. Например: Student.age.between(18, 25) - найдёт студентов в возрасте от 18 до 25 лет.    
+"""
+
+students = Student.select().join(Group).where(
+    ((Student.age > 20) & (Group.group_name == "python411")) | 
+    ((Student.age < 20) & (Group.group_name == "js412"))
+)
+
+
+######################### СОЗДАНИЕ НОВОЙ ЗАПИСИ #########################
+
+# 13. Петр Петрович Петров, 25 лет, группа python411
+
+# new_student = Student.create(
+#     first_name="Петр",
+#     middle_name="Петрович",
+#     last_name="Петров",
+#     age=25,
+#     group=Group.get(Group.group_name == "python411"),
+# )
+
+# 14. Добуду всех студентов и выведу их на экран
+all_students = Student.select()
+
+for student in all_students:
+    print(student.id, student.first_name, student.middle_name, student.last_name, student.age, student.group.group_name)
+
+# У студента id=12 меняю отчество на "Сидорович"
+
+student_12 = Student.get(Student.id == 12)
+student_12.middle_name = "Сидорович"
+student_12.save()  # Сохраняю изменения в базе данных
+
+
+# Проверим изменения
+student_12 = Student.get(Student.id == 12)
+print(student_12.id, student_12.first_name, student_12.middle_name, student_12.last_name, student_12.age, student_12.group.group_name)
